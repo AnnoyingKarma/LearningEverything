9cd8436121d5d569352237735f45f5fcee184b76
Adarsh Sharma <as0040039@gmail.com>
2024-01-19 14:40:48 +0530

    little bit more changes && learning java too

---
+++ Java/JavaDSA/src/Intro/Hello.java
@@ -0,0 +1,21 @@
+package Intro;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+
+        String name="Zamy";
+        System.out.println(name);
+        int mid = 1000000000;
+        System.out.println(mid);
+        char sym = 0x20A0;
+        System.out.println(sym);
+        float track = 10/3f;
+        System.out.println(track);
+        double track1 = 10/3d;
+        System.out.println(track1);
+        long high = 1000000000000000000L;
+        System.out.println(high);
+    }
+}
