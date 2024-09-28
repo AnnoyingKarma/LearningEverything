1a98f8135431713b7ecaa44a3c731453ae34d1fd
Adarsh Sharma <as0040039@gmail.com>
2024-09-28 08:33:20 +0530

    learning recursion

--- Code++/12. Recursion/subsequenceeEqualK.cpp
+++ Code++/12. Recursion/subsequenceeEqualK.cpp
@@ -2,21 +2,18 @@
 #include <vector>
 using namespace std;
 
-int sumK(int sum,int &cnt,int i,int n,int k,vector<int> arr){
+int subSeqEqualK(int i, int cnt, int sum, int n, int k, vector<int>arr){
   if(i>=n){
-    if(sum==k) cnt++;
-    return cnt;
+    return (sum==k)? cnt+1:cnt;
   }
 
-  sumK(sum+arr[i],cnt,i+1,n,k,arr);
-  sumK(sum,cnt,i+1,n,k,arr);
+  cnt=subSeqEqualK(i+1,cnt,sum+arr[i],n,k,arr);
+  cnt=subSeqEqualK(i+1,cnt,sum,n,k,arr);
   return cnt;
 }
 
 int main(){
   vector<int> arr={1,2,1};
-  int n=arr.size(); 
-  int cnt;
-  cout << sumK(0,cnt,0,n,2,arr);
-   
+  vector<int> emp;
+  cout << subSeqEqualK(0,0,0,arr.size(),2,arr);
 }
