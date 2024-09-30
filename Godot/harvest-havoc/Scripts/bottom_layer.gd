4863cd965f42dd6a995a826e7642cd7d168c571c
Adarsh Sharma <as0040039@gmail.com>
2024-09-30 15:03:59 +0530

    some ui done, worked on retry menu and main menu and some stuff still left , made a score manager to managere score , a global script made

--- Godot/harvest-havoc/Scripts/bottom_layer.gd
+++ Godot/harvest-havoc/Scripts/bottom_layer.gd
@@ -4,7 +4,13 @@ var GlobalScreenSize;
 
 func _ready() -> void:
 	GlobalScreenSize=get_viewport_rect().size;
-	var collShape=get_child(0);
-	collShape.position.x=GlobalScreenSize.x/2;
-	collShape.position.y=-GlobalScreenSize.y-50;
-	
+	self.position.x=GlobalScreenSize.x/2;
+	self.position.y=GlobalScreenSize.y+100;
+
+
+func _on_background_scaling_value(scaleValue: Vector2) -> void:
+	self.scale+=scaleValue;
+
+
+func _on_body_entered(body: Node2D) -> void:
+	body.queue_free();
